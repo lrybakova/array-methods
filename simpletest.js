3ce6cc8fa7986c7919d3a69b198e74f42d55c4c3
Mila Rybakova <vista69@gmail.com>
2020-05-15 23:00:25 +0300

    Improved formatting

--- simpletest.js
+++ simpletest.js
@@ -40,7 +40,7 @@
 
 var utils = {
   renderStats: function (successes, failures) {
-    document.body.innerHTML = 'Ran '+ (failures + successes) + ' ' + 'tests: '+' '+successes + ' successes, '+ failures + ' failures';
+    document.body.innerHTML = 'Ran ' + (failures + successes) + ' ' + 'tests: ' + ' ' + successes + ' successes, ' + failures + ' failures';
     document.body.style.fontSize = '22px'
     document.body.style.fontWeight = 'bold'
   }
@@ -48,58 +48,58 @@ var utils = {
 
 var TinyTest = {
 
-  run: function(tests) {
-      var failures = 0;
-      var successes = 0;
-      for (var testName in tests) {
-          var testAction = tests[testName];
-          try {
-              testAction.apply(this);
-              successes++;
-              console.log("%c" + testName, "color: green; font-weight: bold");
-          } catch (e) {
-              failures++;
-              console.groupCollapsed('%c'+ testName + ' ' + e, "color: red; font-weight: bold");
-              console.error(e.stack);
-              console.groupEnd();
-            
-          }
+  run: function (tests) {
+    var failures = 0;
+    var successes = 0;
+    for (var testName in tests) {
+      var testAction = tests[testName];
+      try {
+        testAction.apply(this);
+        successes++;
+        console.log("%c" + testName, "color: green; font-weight: bold");
+      } catch (e) {
+        failures++;
+        console.groupCollapsed('%c' + testName + ' ' + e, "color: red; font-weight: bold");
+        console.error(e.stack);
+        console.groupEnd();
+
       }
-      setTimeout(function() { // Give document a chance to complete
-          if (window.document && document.body) {
-              document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999');
-              utils.renderStats(successes, failures);
-          }
-      }, 0);
+    }
+    setTimeout(function () { // Give document a chance to complete
+      if (window.document && document.body) {
+        document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999');
+        utils.renderStats(successes, failures);
+      }
+    }, 0);
   },
 
-  fail: function(msg) {
-      throw new Error('fail(): ' + msg);
+  fail: function (msg) {
+    throw new Error('fail(): ' + msg);
   },
 
-  assert: function(value, msg) {
-      if (!value) {
-          throw new Error('assert(): ' + msg);
-      }
+  assert: function (value, msg) {
+    if (!value) {
+      throw new Error('assert(): ' + msg);
+    }
   },
 
-  assertEquals: function(expected, actual) {
-      if (expected != actual) {
-          throw new Error('assertEquals() "' + expected + '" != "' + actual + '"');
-      }
+  assertEquals: function (expected, actual) {
+    if (expected != actual) {
+      throw new Error('assertEquals() "' + expected + '" != "' + actual + '"');
+    }
   },
 
-  assertStrictEquals: function(expected, actual) {
-      if (expected !== actual) {
-          throw new Error('assertStrictEquals() "' + expected + '" !== "' + actual + '"');
-      }
+  assertStrictEquals: function (expected, actual) {
+    if (expected !== actual) {
+      throw new Error('assertStrictEquals() "' + expected + '" !== "' + actual + '"');
+    }
   },
 
 };
 
-var fail               = TinyTest.fail.bind(TinyTest),
-  assert             = TinyTest.assert.bind(TinyTest),
-  assertEquals       = TinyTest.assertEquals.bind(TinyTest),
-  eq                 = TinyTest.assertEquals.bind(TinyTest), // alias for assertEquals
+var fail = TinyTest.fail.bind(TinyTest),
+  assert = TinyTest.assert.bind(TinyTest),
+  assertEquals = TinyTest.assertEquals.bind(TinyTest),
+  eq = TinyTest.assertEquals.bind(TinyTest), // alias for assertEquals
   assertStrictEquals = TinyTest.assertStrictEquals.bind(TinyTest),
-  tests              = TinyTest.run.bind(TinyTest);
+  tests = TinyTest.run.bind(TinyTest);
